259420f45fb612512c8633aa33ce881d99ee6964
Sávio Fialho <savioabfialho@gmail.com>
2021-04-27 10:02:39 +0100

    clipboard working

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const topLeft = document.getElementById("topLeft");
 const topRight = document.getElementById("topRight");
 const bottomRight = document.getElementById("bottomRight");
 const bottomLeft = document.getElementById("bottomLeft");
-const initialValue = 0
+const copyBtn = document.getElementById("copyBtn")
+const initialValue = 0;
 const cssProperty = document.getElementById('cssProperty');
 let cssInstructions = [];
 
@@ -15,13 +16,13 @@ bottomLeft.value = initialValue;
 
 const changeBorderRadius = position => {
 
+    resetClipboard();
+
     if(position === 'topLeft'){
         box.style.borderTopLeftRadius = topLeft.value + 'px';
-        updateClipBoard('border-top-left-radius', topLeft.value);
     } 
     else if(position === 'topRight'){
         box.style.borderTopRightRadius = topRight.value + 'px';
-        updateClipBoard('border-top-right-radius', topRight.value);
     }
     else if(position === 'bottomRight'){
         box.style.borderBottomRightRadius = bottomRight.value + 'px';
@@ -29,16 +30,37 @@ const changeBorderRadius = position => {
     else if(position === 'bottomLeft'){
         box.style.borderBottomLeftRadius = bottomLeft.value + 'px';
     }
+
+    updateClipBoard();
+
+    
 }
 
-const updateClipBoard = (attribute, value) => {
-    cssInstructions.push(`${attribute}:${value};`)
-    cssInstructions.forEach(e => {
-        cssProperty
+const updateClipBoard = () => {
+
+    cssInstructions.push(`border-top-left-radius: ${topLeft.value}px;`, `border-top-right-radius: ${topRight.value}px;`, `border-bottom-right-radius: ${bottomRight.value}px;`, `border-bottom-left-radius: ${bottomLeft.value}px;`);
+
+    cssInstructions.forEach(element => {
+        cssProperty.value += element;    
     });
+    console.log(cssInstructions);
+
+}
+
+const resetClipboard = () => {
+    cssProperty.value = '';
+    cssInstructions = [];
+}
+
+const copyToClipboard = () => {
+    cssProperty.select();
+    cssProperty.setSelectionRange(0,9999);
+    document.execCommand("copy");
+    alert('CSS copied to clipboard')
 }
 
 topLeft.addEventListener('change', changeBorderRadius.bind(null, 'topLeft'));
 topRight.addEventListener('change', changeBorderRadius.bind(null, 'topRight'));
 bottomRight.addEventListener('change', changeBorderRadius.bind(null, 'bottomRight'));
 bottomLeft.addEventListener('change', changeBorderRadius.bind(null, 'bottomLeft'));
+copyBtn.addEventListener('click', copyToClipboard);
